628e14f1463b95a435387be12f77ac51e834a0d3
Mark Nash <abacefabmicroblast@hotmail.com>
2018-05-22 22:43:34 -0400

    Added first scren buttons

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,12 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource
+                ("methodOfGameplay.fxml"));
+        primaryStage.setTitle("Select Gameplay Method");
+        primaryStage.setScene(new Scene(root, 300, 400));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
